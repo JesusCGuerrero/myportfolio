05ffc8a58b1f063bdfa0acad7f11491583f08cb0
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-10-06 22:40:01 -0400

    removed target blanks

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -8,9 +8,9 @@ export const LeftSide = () => {
     return (
         <div className="left-side">
             <div className="icon-box">
-                <a target="_blank" href="https://github.com/JesusCGuerrero"><AiFillGithub className="icon-github"/></a>
-                <a target="_blank" href="https://www.linkedin.com/in/JesusCGuerrero"><AiOutlineLinkedin className="icon-linkedin"/></a>
-                <a target="_blank" href="https://codepen.io/JesusGuerrero"><AiOutlineCodepen className="icon-codepen"/></a>
+                <a href="https://github.com/JesusCGuerrero"><AiFillGithub className="icon-github"/></a>
+                <a href="https://www.linkedin.com/in/JesusCGuerrero"><AiOutlineLinkedin className="icon-linkedin"/></a>
+                <a href="https://codepen.io/JesusGuerrero"><AiOutlineCodepen className="icon-codepen"/></a>
             </div>
             <div className="left-line"></div>
         </div>
